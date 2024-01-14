561180ae3fd94070b504eed74122b342a7b2b39b
Umar <us42534@gmail.com>
2024-01-15 01:40:08 +0530

    second

--- src/Components/SliderIffect.jsx
+++ src/Components/SliderIffect.jsx
@@ -19,11 +19,26 @@ function SliderIffect() {
     <div className='Slider-min-div'>
       <Slider {...settings}>
         <div className='image-div'>
-          <img className='slider-image' src="https://media.islamicity.org/wp-content/uploads/2021/04/iStock-840887390.jpg" alt="" />
+          <div className='image1'>
+
+          <img className='slider-image image1' src="https://media.islamicity.org/wp-content/uploads/2021/04/iStock-840887390.jpg" alt="" />
+          </div>
+          <div className='image2'>
+
+          <img  className='slider-image image2' src="https://images.unsplash.com/photo-1564769625905-50e93615e769?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bWVjY2ElMjBrYWFiYXxlbnwwfHwwfHx8MA%3D%3D" alt="" />
+          </div>
         </div>
         
         <div className='image-div'>
+          <div className='image1'>
+
           <img className='slider-image' src="https://cdn.britannica.com/86/77986-050-ADB6ED09/site-Prophets-Mosque-Medina-Saudi-Arabia-Mecca.jpg" alt="" />
+          </div>
+
+          <div className='image2'>
+          <img className='slider-image image2' src="https://w0.peakpx.com/wallpaper/395/704/HD-wallpaper-madina-42-masjid-mosque.jpg" alt="" />
+          </div>
+
         </div>
       </Slider>
 
